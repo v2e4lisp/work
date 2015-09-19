e5952ef352e3b866b1c29d27b82a44f0079a805f
wenjun.yan <mylastnameisyan@gmail.com>
2015-09-19 17:16:23 +0900

    fix

--- example_test.go
+++ example_test.go
@@ -9,18 +9,18 @@ import (
         "github.com/v2e4lisp/work"
 )
 
-// Worker function
-func addOne(i interface{}) interface{} {
-        j := i.(int) + 1
-
-        rand.Seed(time.Now().UnixNano())
-        t := rand.Intn(300)
-        time.Sleep(time.Duration(t) * time.Millisecond)
-
-        return j
-}
-
 func Example() {
+        // Worker function
+        addOne := func(i interface{}) interface{} {
+                j := i.(int) + 1
+
+                rand.Seed(time.Now().UnixNano())
+                t := rand.Intn(300)
+                time.Sleep(time.Duration(t) * time.Millisecond)
+
+                return j
+        }
+
         // handle two jobs at the same time
         in, out := work.Start(addOne, 2)
         var results []int
